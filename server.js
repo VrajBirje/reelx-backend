750f6394c39e6b205a1d49028f98d284d27c1a31
VrajBirje <vrajbirje0309@gmail.com>
2025-03-01 18:08:19 +0530

    cors added 3000

--- server.js
+++ server.js
@@ -10,14 +10,16 @@ const cartRoutes = require("./routes/cartRoutes");
 const paymentRoutes = require('./routes/razorRoutes');
 
 // Allow requests from specific origin (frontend domain)
-// const allowedOrigins = ['https://shree-vidya-saraswati-pujan.netlify.app'];
-const allowedOrigins = ['https://reelxclothing.netlify.app'];
+const allowedOrigins = [
+  'https://reelxclothing.netlify.app',
+  'http://localhost:3000' // Add localhost:3000 to the allowed origins
+];
+
 app.use(cors({
   origin: function (origin, callback) {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
-      // console.log("errorrrr...")
       callback(new Error('Not allowed by CORS'));
     }
   },
@@ -30,20 +32,18 @@ app.get('/', (req, res) => {
   res.send('Welcome to the Mastermind Server.');
 });
 
-
 app.use('/api/products',  productRoutes);
 app.use('/api/raw-tshirts',  raw_tshirtsRoutes);
 app.use("/api/wishlist", wishlistRoutes);
 app.use("/api/cart", cartRoutes);
 app.use('/api/payment', paymentRoutes);
 
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-//Following code for running the server on specific network i.e. IP
+// Following code for running the server on specific network i.e. IP
 // app.listen(PORT, '10.112.9.12', () => {
 //     console.log(`Server running on port ${PORT}`);
-//   });
+// });
