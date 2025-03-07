981e321d65d527411164acd6e72eba764242421f
VrajBirje <vrajbirje0309@gmail.com>
2025-03-07 11:09:21 +0530

    order api fix

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -3,13 +3,13 @@ const orderService = require('../services/orderService');
 // 🔵 Create Order
 exports.createOrder = async (req, res) => {
     try {
-        const { user_id, customer_name, address_details, contact_details, providedSubtotal, providedTotal, discounted_amount, shipping_charges, payment_method, coupon_code } = req.body;
+        const { user_id, customer_name, address_details, contact_details, providedSubtotal, providedTotal, providedDiscountedAmount, shipping_charges, payment_method, coupon_code } = req.body;
 
         if (!user_id || !customer_name || !address_details || !contact_details || !shipping_charges || !payment_method) {
             return res.status(400).json({ success: false, message: "Missing required fields." });
         }
 
-        const order = await orderService.createOrder(user_id, customer_name, address_details, contact_details, providedSubtotal, providedTotal, discounted_amount, shipping_charges, payment_method, coupon_code);
+        const order = await orderService.createOrder(user_id, customer_name, address_details, contact_details, providedSubtotal, providedTotal, providedDiscountedAmount, shipping_charges, payment_method, coupon_code);
         res.status(201).json({ success: true, order });
 
     } catch (error) {
